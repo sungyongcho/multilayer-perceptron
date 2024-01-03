d6ac36d825d291b0329dab6369571d6bff59dc7f
Sungyong Cho <dev@sungyongcho.com>
2024-01-03 14:35:30 +0100

    wip: removing comments

--- srcs/neural_network.py
+++ srcs/neural_network.py
@@ -173,30 +173,14 @@ class NeuralNetwork:
         return self.outputs[-1]
 
     def backpropagation(self, target_row, y_true):
-        # print(self.weights[-1].T)
-        # Calculate the error and delta for the output layer
-        # print(target_row)
         if self.layers[-1].activation == "softmax":
-            # print(len(target_row), y_true)
             self.deltas[-1] = softmax_derivative(target_row, y_true)
-            # self.weights[-1] -= self.lr * np.dot(self.outputs[0].T, self.deltas[-1])
             di = np.dot(self.deltas[-1], self.weights[-1].T)
             self.weights[-1] -= self.lr * np.dot(self.outputs[0].T, self.deltas[-1])
-            # self.weights[-1] -= self.lr * np.dot(self.outputs[0].T, self.deltas[-1])
         elif self.layers[-1].activation == "sigmoid":
             error = -(target_row - self.outputs[-1])
             self.deltas[-1] = error * sigmoid_derivative(self.outputs[-1])
 
-        # dw_1 = np.dot(self.layers[1].inputs.T, self.deltas[1])
-        # # dw_1 = np.dot(np.squeeze(np.array(self.layers[1].inputs)).T, self.deltas[-1])
-        # di_1 = np.dot(self.deltas[1], self.weights[1].T)
-
-        # di_0 = di_1.copy()
-        # di_0[self.layers[1].inputs <= 0] = 0
-
-        # dw_0 = np.dot(self.layers[0].inputs.T, di_0)
-        # print(dw_0)
-        # print(np.dot(self.deltas[1], self.weights[1].T))
         for i in reversed(range(len(self.weights) - 1)):
             if self.layers[i + 1].activation == "relu":
                 self.deltas[i] = relu_derivative(di, self.layers[i + 1].inputs)
@@ -210,35 +194,6 @@ class NeuralNetwork:
                 ) * sigmoid_derivative(self.outputs[i])
             elif self.layers[i + 1].activation == "softmax":
                 self.deltas[i] = np.dot(self.deltas[i + 1], self.weights[i + 1].T)
-            # self.weights[i] -= self.lr * np.dot(self.layers[i].inputs.T, self.deltas[i])
-
-        # print(self.layers[1].inputs)
-        # print(self.weights[1])
-        # print(self.deltas[0])
-        # print(self.layers[1].inputs)
-        # print(self.outputs[-2])
-        # print(np.array(self.outputs[-2]))
-        # # Backpropagate the error to previous layers
-        # for i in reversed(range(len(self.deltas) - 1)):
-        #     error = np.dot(self.deltas[i + 1], self.weights[i + 1].T)
-        #     if self.layers[i + 1].activation == "softmax":
-        #         self.deltas[i] = error * sigmoid_derivative(self.outputs[i])
-        #     elif self.layers[i + 1].activation == "relu":
-        #         self.deltas[i] = error * relu_derivative(self.outputs[i])
-
-        # print(self.outputs[-2])
-        # # Update the weights and biases for each layer
-        # for i in range(len(self.weights)):
-        #     if i == 0:
-        #         self.weights[i] -= self.lr * np.dot(
-        #             y_true.reshape(-1, 1), self.deltas[i].reshape(1, -1)
-        #         )
-        #         self.biases[i] -= self.lr * np.sum(self.deltas[i], axis=0)
-        #     else:
-        #         self.weights[i] -= self.lr * np.dot(
-        #             self.outputs[i - 1].reshape(-1, 1), self.deltas[i].reshape(1, -1)
-        #         )
-        #         self.biases[i] -= self.lr * np.sum(self.deltas[i], axis=0)
 
     def plot_graphs(self, train_loss_history, valid_loss_history):
         fig = plt.figure(figsize=(10, 5))
@@ -272,28 +227,11 @@ class NeuralNetwork:
     def fit(
         self, layers, data_train, data_valid, loss, learning_rate, batch_size, epochs
     ):
-        # X_train = data_train.drop(data_train.columns[0], axis=1).to_numpy()
-        # y_train = (
-        #     (data_train[data_train.columns[0]] == "M")
-        #     .astype(int)
-        #     .to_numpy()
-        #     .reshape(-1, 1)
-        # )
-
-        # X_valid = data_valid.drop(data_valid.columns[0], axis=1).to_numpy()
-        # y_valid = (
-        #     (data_valid[data_valid.columns[0]] == "M")
-        #     .astype(int)
-        #     .to_numpy()
-        #     .reshape(-1, 1)
-        # )
-
         # Load X_train from the CSV file
         X_train = np.loadtxt("X_train.csv", delimiter=",")
 
         # Load y_train from the CSV file
         y_train = np.loadtxt("y_train.csv", delimiter=",", dtype=int)
-        # print(X_train)
         self.lr = learning_rate
         if self.layers is None and layers is not None:
             self.__init__(layers)
@@ -303,35 +241,8 @@ class NeuralNetwork:
         for epoch in range(epochs):
             self.iamchecking = []
             train_epoch_loss = 0
-            # for i in range(X_train.shape[0]):
             self.feedforward(X_train)
-            # print(np.array(self.iamchecking))
             self.iamchecking = np.squeeze(np.array(self.iamchecking))
             loss = np.mean(crossentropy(y_train, self.iamchecking))
             print(loss)
-            # print(self.accuracy(y_train, np.array(self.iamchecking)))
-            # for i in range(X_train.shape[0]):
             self.backpropagation(np.array(self.iamchecking), y_train)
-            #     loss = self.mse_loss(y_train[i], self.outputs[-1])
-            #     train_epoch_loss += loss
-            # avg_epoch_train_loss = train_epoch_loss / X_train.shape[0]
-            # train_loss_history.append(avg_epoch_train_loss)
-            # print(
-            #     f"Epoch {epoch}, Average Loss: {avg_epoch_train_loss}",
-            #     X_train.shape[0],
-            # )
-        # print(self.weights[-1])
-        # Validation loss
-        #     valid_epoch_loss = 0
-
-        #     for i in range(data_train.shape[0]):
-        #         self.feedforward(X_valid[i])
-        #         loss = self.mse_loss(y_valid[i], self.outputs[-1])
-        #         valid_epoch_loss += loss
-
-        #     avg_valid_epoch_loss = valid_epoch_loss / data_valid.shape[0]
-        #     valid_loss_history.append(avg_valid_epoch_loss)
-
-        # self.plot_graphs(train_loss_history, valid_loss_history)
-
-        # print(self.weights)
