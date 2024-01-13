fix: activation derivative functions fixed

nnfs_16.py
@@ -154,7 +154,9 @@ class Activation_Sigmoid:
     # Backward pass
     def backward(self, dvalues):
         # Derivative - calculates from output of the sigmoid function
+        # print(self.output)
         self.dinputs = dvalues * (1 - self.output) * self.output
+        # print(self.dinputs)
 
 
 # SGD optimizer
@@ -576,7 +578,7 @@ y = np.loadtxt("./nnfs_data/y_train_16.csv", delimiter=",").reshape(-1, 1)
 dense1 = Layer_Dense(2, 64)
 
 # Create ReLU activation (to be used with Dense layer):
-activation1 = Activation_ReLU()
+activation1 = Activation_Sigmoid()
 
 # Create second Dense layer with 64 input features (as we take output
 # of previous layer here) and 1 output value
@@ -598,7 +600,7 @@ loss_function = Loss_BinaryCrossentropy()
 optimizer = Optimizer_Adam(decay=5e-7)
 
 # Train in loop
-for epoch in range(5):
+for epoch in range(1001):
     # Perform a forward pass of our training data through this layer
     dense1.forward(X)
     # print(X)

srcs/dense_layer.py
@@ -1,5 +1,13 @@
 import numpy as np
-from srcs.utils import sigmoid, softmax, relu, relu_deriv, heUniform
+from srcs.utils import (
+    sigmoid,
+    sigmoid_deriv,
+    relu,
+    relu_deriv,
+    softmax,
+    softmax_deriv,
+    heUniform,
+)
 
 
 def softmax_derivative(softmax_output):
@@ -24,15 +32,18 @@ class DenseLayer:
 
         if self.activation == "sigmoid":
             self.activation_function = lambda x: sigmoid(x)
-            self.activation_deriv = lambda x: x * (1 - x)
+            self.activation_deriv = lambda gradient: sigmoid_deriv(
+                self.outputs, gradient
+            )
         elif self.activation == "softmax":
             self.activation_function = lambda x: softmax(x)
             # TODO
-            self.activation_deriv = lambda x: np.multiply(x, (1 - x))
+            self.activation_deriv = lambda gradient: softmax_deriv(
+                self.outputs, gradient
+            )
         elif self.activation == "relu":
             self.activation_function = lambda x: relu(x)
-            # TODO
-            self.activation_deriv = lambda y_true, y_pred: relu_deriv(y_true, y_pred)
+            self.activation_deriv = lambda gradient: relu_deriv(self.inputs, gradient)
 
     def __str__(self):
         return f'DenseLayer - Shape: {self.shape}, Activation: "{self.activation}", Weights Initializer: {self.weights_initializer}'
@@ -54,5 +65,5 @@ class DenseLayer:
     def set_outputs(self, x):
         self.outputs = self.activation_function(x)
 
-    def set_activation_gradient(self, y_true, y_pred):
-        self.deltas = self.activation_deriv(y_true, y_pred)
+    def set_activation_gradient(self, gradient):
+        self.deltas = self.activation_deriv(gradient)

srcs/neural_network.py
@@ -84,6 +84,8 @@ class NeuralNetwork:
             self.layers[i].init_biases()
 
     def _assign_optimizer_class(self, optimizer, learning_rate, decay):
+        if optimizer != "sgd" and optimizer != "adam":
+            raise ValueError("optimizer not set correctly.")
         if optimizer == "sgd":
             self.optimizer = Optimizer_SGD(learning_rate=learning_rate, decay=decay)
         elif optimizer == "adam":
@@ -146,16 +148,13 @@ class NeuralNetwork:
         # update activation gradients (delta)
         for i in reversed(range(1, len(self.layers) - 1)):
             # print(i)
-            if self.layers[i].activation == "relu":
-                self.layers[i].set_activation_gradient(self.layers[i].inputs, error)
-            elif self.layers[i].activation == "sigmoid":
-                pass
-                # self.deltas[i] = np.dot(
-                #     self.deltas[i + 1], self.weights[i + 1].T
-                # ) * sigmoid_derivative(self.outputs[i])
-            elif self.layers[i].activation == "softmax":
-                pass
-                # self.deltas[i] = softmax_derivative(di, self.layers[i + 1].inputs)
+            self.layers[i].set_activation_gradient(error)
+            # elif self.layers[i].activation == "sigmoid":
+            #     # print(error)
+            #     self.layers[i].set_activation_gradient(error)
+            # elif self.layers[i].activation == "softmax":
+            #     pass
+            #     # self.deltas[i] = softmax_derivative(di, self.layers[i + 1].inputs)
 
             error = np.dot(self.layers[i].deltas, self.layers[i].weights.T)
 

srcs/utils.py
@@ -242,6 +242,25 @@ def heUniform(shape):
     return np.random.uniform(low=-limit, high=limit, size=shape)
 
 
-def relu_deriv(y_true, y_pred):
-    y_pred[y_true <= 0] = 0
-    return y_pred
+def sigmoid_deriv(outputs, gradient):
+    return gradient * (1 - outputs) * outputs
+
+
+def relu_deriv(inputs, gradient):
+    gradient[inputs <= 0] = 0
+    return gradient
+
+
+def softmax_deriv(outputs, gradient):
+    deriv = np.empty_like(gradient)
+
+    for index, (single_output, single_dvalues) in enumerate(zip(outputs, gradient)):
+        single_output = single_output.reshape(-1, 1)
+        jacobian_matrix = np.diagflat(single_output) - np.dot(
+            single_output, single_output.T
+        )
+        # Calculate sample-wise gradient
+        # and add it to the array of sample gradients
+        deriv[index] = np.dot(jacobian_matrix, single_dvalues)
+
+    return deriv